04b438a75f2b071bd311da41f15c89b86fcc8e9a
siti <sfati1342@gmail.com>
2025-02-04 15:16:20 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn company_attendance.wsgi
--- company_attendance/settings.py
+++ company_attendance/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-j7!vgis6y3(yaox!rmtygx2jtub%j-^g-@tpw!x@#b4lltl!ey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -117,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
